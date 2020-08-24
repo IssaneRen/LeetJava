897b8f932976b5ba97f46e7bbb250cf2695d26b8
hongfei_ren <hongfei_ren@intsig.net>
2020-08-24 18:40:38 +0800

    初始提交

---
+++ src/util/CommonUtils.java
@@ -0,0 +1,22 @@
+package util;
+
+public class CommonUtils {
+    public static String  array2String(int[] array, int length) {
+        if (array == null)
+            return "NULL";
+        int finalLength = Math.min(length, array.length);
+        if (finalLength == 1)
+            return "{" + array[0] + "}";
+        StringBuilder builder = new StringBuilder("{");
+        builder.append(array[0]);
+        for (int i = 1; i < finalLength; i++) {
+            builder.append(", ");
+            builder.append(array[i]);
+        }
+        builder.append("}");
+        return builder.toString();
+    }
+    public static String  array2String(int[] array) {
+        return array2String(array, array != null ? array.length : 0);
+    }
+}
